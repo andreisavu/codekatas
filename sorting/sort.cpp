1d0357d048908b6ed5376b79ca8a52e6899a3b39
Andrei Savu <savu.andrei@gmail.com>
2010-05-22 01:03:49 +0300

    refactored the C code

--- sorting/sort.cpp
+++ sorting/sort.cpp
@@ -27,19 +27,32 @@ int check(int seq[], int n) {
     return 1;
 }
 
-int main() {
+void test_sort(char *msg, void (*sort_fn)(int seq[], int n) ) {
+    std::cout << "Testing: " << msg << " .... ";
 
     int reverse_order[] = {4,3,2,1};
+    int random_order[] = {3,7,1,5};
     int n = 4;
 
-    insertion_sort(reverse_order, n);
+    sort_fn(reverse_order, n);
     assert(check(reverse_order, n));
+
+    sort_fn(random_order, n);
+    assert(check(random_order, n));
 
     int empty_sequence[] = {};
     
-    insertion_sort(empty_sequence, 0);
+    sort_fn(empty_sequence, 0);
     assert(check(empty_sequence, 0));
 
-    return 0;
+    int single_element[] = {1};
+    sort_fn(single_element, 1);
+    assert(check(single_element, 1));
+
+    std::cout << "Ok." << std::endl;
 }
 
+int main() {
+    test_sort("Insertion Sort", insertion_sort);
+    return 0;
+}
